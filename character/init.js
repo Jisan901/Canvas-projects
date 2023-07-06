9dfce42c1e5704fdf7230efb72c0a723e60399bd
Jisan901 <jisuna901@gmail.com>
2023-07-06 20:02:19 +0600

    fix

--- character/init.js
+++ character/init.js
@@ -67,15 +67,49 @@ canvas.height = 720;
 canvas.width = 800;
 const ctx = canvas.getContext('2d');
 
+let collider;
+
+let collisionData = {
+    initial:false,
+    x:null,
+    y:null
+}
+
+
 const img = new Image()
 const img2 = new Image()
 const img3 = new Image()
 const img4 = new Image()
+const img5 = new Image()
 img.src="./a.png"
 img2.src="./background.png"
 img3.src="./enemy2.png"
 img4.src="./enemy4.png"
+img5.src="./boom.png"
+
+
+
+
+
+let randPic  = [{img:img3,frames:6,h:188,w:266},{img:img4,frames:9,w:212,h:213}]
+
 window.onload = ()=>{
+    
+collider = new Character(
+    ctx,
+    canvas,
+    -102,
+    -102,
+    1,
+    200,
+    179,
+    img5,
+    "i1",
+    [{name:'i1',frames:5}],
+    3
+    )
+
+
 background = new Background(ctx,img2,2400,720,5)
 let adjustY = 250;
 plyer = new Character(
@@ -104,6 +138,9 @@ plyer = new Character(
             event=null
         })
     })
+    
+    plyer.options.energy = 100
+    
     plyer.options.speed = 0;
     plyer.options.vy = 0;
     plyer.options.weight = 1;
@@ -119,27 +156,31 @@ plyer = new Character(
     function handleEnemy(deltaTime){
         
         if (enemyTimer > enemyInterval+randomInterval) {
+            let enemyRand = randPic[Math.round(Math.random())]
             enemyArray.push(new Character(
         ctx,
         canvas,
         canvas.width+212/2,
-        canvas.height-212/2,
+        canvas.height-215/2,
         0.7,
-        213,
-        212,
-        img4,
+        enemyRand.w,
+        enemyRand.h,
+        enemyRand.img,
         "ip",
-        [{name:"ip",frames:9}],
-        5
+        [{name:"ip",frames:enemyRand.frames}],
+        3
         ))
-            randomInterval=Math.random()*500+1000;
+            randomInterval=Math.random()*500+800;
             enemyTimer=0
         }
         else{
             enemyTimer+=deltaTime
         }
         enemyArray.forEach(enemy=>{
-        enemy.x-=Math.random()*8+5
+            
+            
+        enemy.x-=Math.random()*10+10 // enemy speed
+        
         if (enemy.x<0-enemy.width*enemy.size*0.5) {
             enemy.options.markForDeletion = true;
             score++
@@ -170,11 +211,14 @@ plyer = new Character(
         }else if(event==="btn_down"&&onGround(player)){
             player.dispatch('i6')
             background.speed=0
-        }else if(event==="btn_down"&&!onGround(player)){
+            player.options.energy>100?player.options.energy=100:player.options.energy+=0.5
+        }else if(event==="btn_down"&&!onGround(player)&&player.options.energy>10){
             player.dispatch('i7')
             player.options.vy+=4
-        }else if(event==="btn_action"){
+            player.options.energy<10?player.options.energy=9:player.options.energy--
+        }else if(event==="btn_action"&&player.options.energy>10){
             player.dispatch('i7')
+            player.options.energy<10?player.options.energy=9:player.options.energy-=0.5
         }
         else{
             player.options.speed=0;
@@ -226,7 +270,11 @@ plyer = new Character(
             if (distance<enemy.hitBoxHeight*enemy.hitboxscale/2+player.hitBoxHeight*player.hitboxscale/2) {
                 if (player.state==='i7') {
                     enemy.options.markForDeletion=true
-                    score++
+                    score+=2
+                    collider.stop()
+                    collider.x=enemy.x
+                    collider.y=enemy.y
+                    collider.run()
                 }
                 else{
                     
@@ -256,7 +304,25 @@ plyer = new Character(
         plyer.createHitBox(plyer.x,plyer.y,plyer.width,plyer.height,plyer.size)
         plyer.draw()
         plyer.update()
-        
+        ctx.font = "70px Aerial"
+        ctx.fillText("score: "+score,70,70)
+        ctx.font = "30px Aerial"
+        ctx.fillText("energy: "+plyer.options.energy,canvas.width-200,60)
+        // if (collisionData.initial) {
+        //     collider.x=collisionData.x
+        //     collider.y=collisionData.y
+        //     collider.draw()
+        //     collider.update()
+        //     if (collider.frame===5) {
+        //         collisionData.initial=false
+        //     }
+        // }
+        if (collider.frame===4) {
+            collider.x=-100
+            collider.y=-100
+        }
+        collider.draw()
+        collider.update()
         handlePlayer(plyer)
         handleEnemy(deltaTime)
        //plyer.drawHitBox("#ff0",5)
